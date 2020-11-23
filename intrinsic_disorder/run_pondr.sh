b9811fc97013bd85e014889d5cbdc1df33a63e1f
mjnur <mjnur@ucdavis.edu>
2020-11-22 22:03:18 -0800

    edited run_pondr to use jar file in current directory and work on macOS

--- intrinsic_disorder/run_pondr.sh
+++ intrinsic_disorder/run_pondr.sh
@@ -6,7 +6,7 @@
 #loop through $input_proteins, get each gene (without fasta header)
 #send each gene to PONDR, append results to $output_results
 #
-#PONDR only runs 1 AA seq at a time, 
+#PONDR only runs 1 AA seq at a time,
 #    PONDR doesn't allow any  special chars (*) or fasta headers
 
 
@@ -15,30 +15,28 @@ output_results="$2"
 num_proteins=$(grep -c ">" $input_proteins)
 
 
-#rm -f -- $output_results && touch $output_results
+rm -f -- $output_results && touch $output_results
 
 for (( gene=1; gene <= num_proteins; ((gene++)) ))
 do
     echo -e "processing gene: $gene of $num_proteins"
-    
+
     #gene n will be at file_pos 2n (fasta file is a single line fasta file format)
     gene_file_pos=$(( 2 * $gene ))
-    
+
     #put gene in temp file
     sed -n -e ${gene_file_pos}p $input_proteins > tempGene
 
     #run PONDR, get raw results in tempResults
-    java -jar /share/rwmwork/mjnur/Software/bin/VSL2.jar -s:tempGene > tempResults
+    java -jar VSL2.jar -s:tempGene > tempResults
 
     #remove unnecessary headers in PONDR results and delete last line
-    sed -n -i '/------/,$p' tempResults && sed -i '$d' tempResults
+    sed -n -iE '/------/,$p' tempResults && sed -iE '$d' tempResults
 
     #append to user output file, for easy processing in R (comment char = '-')
-    cat tempResults >> $output_results 
+    cat tempResults >> $output_results
 
 done
 
-rm tempGene
-rm tempResults
-
-
+rm tempGene*
+rm tempResults*
